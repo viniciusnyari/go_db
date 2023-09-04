76b1e98fee709ba11fa0d406e6498537bd9ad577
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-09-04 11:15:52 -0300

    Criando base de dados em GO

---
+++ README.md
@@ -0,0 +1,19 @@
+# Acessando banco de dados com Go
+
+## Subindo Docker
+- docker-compose up -d
+
+## Acessando o MySQL no container
+- docker-compose exec mysql bash
+- mysql -uroot -p goexpert
+
+## Criar tabela
+- create table products (id varchar(255), name varchar(80), price decimal(10,2), primary key(id));	
+
+## Comandos diversos MySQL
+- show tables;
+- describe products;
+- select * from products
+
+## Executando GO
+- go run main.go;
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func newProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Price: price,
+	}
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	product := newProduct("Notebook", 1899.90)
+
+	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("insert into products(id, name, price) values (?,?,?)")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
